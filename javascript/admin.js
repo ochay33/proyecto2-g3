8fbf949cf39b5a9a8cf0c8e706cd898ddf7ef164
¨´Nadir <elochay@gmail.com>
2023-04-13 21:14:59 -0300

    ultimoNadir3

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,5 +1,10 @@
+let list = JSON.parse(localStorage.getItem("lista")) || [];
 let h1 = document.querySelector("h1");
 h1.innerText = "Administrador";
+
+// if (localStorage.getItem("logged", false)) { 
+//     window.location = "../pages/login.html";
+// }
 
 let h3 = document.querySelector("h3");
 h3.innerText = "Productos";
@@ -10,27 +15,34 @@ button.innerText = "AGREGAR PRODUCTO";
 let table = document.querySelector("table");
 let tbody = document.querySelector("tbody");
 
-function agregarProducto() {
-    let tr = document.createElement("tr");
-    tbody.appendChild(tr);
-    let td = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-    let td5 = document.createElement("td");
-    let td6 = document.createElement("td");
-    let td7 = document.createElement("td");
-    tr.appendChild(td);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-    tr.appendChild(td6);
-    tr.appendChild(td7);
-    let button = document.createElement("button");
-    td6.appendChild(button);
-    button.innerText = "Editar";
-    let button2 = document.createElement("button");
-    td7.appendChild(button2);
-    button2.innerText = "Eliminar";
+const agregarProducto = () => {
+    button.addEventListener("click", (event) => { 
+        event.preventDefault();})
+    let id = document.querySelector("#id")
+    let codigo = document.querySelector("#codigo")
+    let nombre = document.querySelector("#nombre")
+    let precio = document.querySelector("#precio")
+    let stock = document.querySelector("#stock")
+    list.push({id: id.value, codigo: codigo.value, nombre: nombre.value, precio: precio.value, stock: stock.value})
+    let listaMapeada = list.map(fila => `<tr><td>${fila.id}</td><td>${fila.codigo}</td><td>${fila.nombre}</td><td>${fila.precio}</td><td>${fila.stock}</td><td><button onclick="editarFila()">Editar</button></td><td><button onclick="eliminarFila(${list.indexOf(fila)})">Eliminar</button></td></tr>`);
+    tbody.innerHTML = listaMapeada.join().replaceAll(",", "")
+    localStorage.setItem("lista", JSON.stringify(list));
+    id.value = "";
+    codigo.value = "";
+    nombre.value = "";
+    precio.value = "";
+    stock.value = "";
+}   
+const editarFila = () => {    
+    let listaMapeada = list.map(fila => `<tr><td contenteditable>${fila.id}</td><td contenteditable>${fila.codigo}</td><td contenteditable>${fila.nombre}</td><td contenteditable>${fila.precio}</td><td contenteditable>${fila.stock}</td><td><button onclick="editarFila()">Editar</button></td><td><button onclick="eliminarFila()">Eliminar</button></td></tr>`);
+    tbody.innerHTML = listaMapeada.join().replaceAll(",", "")
 }
+
+
+const eliminarFila = (indice) => {
+    let newList = [...list];
+    newList.splice(indice, 1);
+    list = newList;
+    let listaMapeada = list.map(fila => `<tr><td>${fila.id}</td><td>${fila.codigo}</td><td>${fila.nombre}</td><td>${fila.precio}</td><td>${fila.stock}</td><td>${editando ? '' : '<button onclick="editarFila()">Editar</button>'}</td><td>${editando ? '' : `<button onclick="eliminarFila(${list.indexOf(fila)})">Eliminar</button>`}</td></tr>`);
+    tbody.innerHTML = listaMapeada.join().replaceAll(",", "");
+  }
--- javascript/registro.js
+++ javascript/registro.js
@@ -1,16 +1,15 @@
-function login() {
+function registro() {
 
     const email = document.getElementById("email").value;
 	const password = document.getElementById("password").value;
 
 	if (email === "leonardodiaz96@hotmail.com" && password === "1234") {
-		window.location = "../index.html";
-        alert("Bienvenido usuario " + email)
+		window.location = "../pages/login.html";
+        alert("Logueate por favor");
 	}
 	else if (email === "elochay24@gmail.com" && password === "4321") {
-		window.location = "../pages/admin.html";
-		alert("Bienvenido usuario " + email);
-		localStorage.setItem("logged", true);
+		window.location = "../pages/login.html";
+		alert("Logueate por favor");
 	} 
 	else {
 		alert("Los datos ingresados son incorrectos")
